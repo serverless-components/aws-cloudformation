ce87b0607f28de9a77125654ededb1321c29bb1b
Eetu Tuomala <eetu.tuomala@maas.global>
2019-08-09 22:18:40 +0300

    create or update stack

--- serverless.js
+++ serverless.js
@@ -1,7 +1,14 @@
 const { Component } = require('@serverless/core')
-const { isNil, mergeDeepRight } = require('ramda')
+const { isNil, merge, mergeDeepRight, not } = require('ramda')
 
-const { getClients, createOrUpdateStack } = require('./utils')
+const {
+  needsUpdate,
+  getClients,
+  fetchOutputs,
+  constructTemplateS3Key,
+  createOrUpdateStack,
+  uploadTemplate
+} = require('./utils')
 
 const defaults = {
   region: 'us-east-1'
@@ -10,9 +17,12 @@ const defaults = {
 class AwsCloudFormation extends Component {
   async default(inputs = {}) {
     this.context.status(`Deploying`)
-
     const config = mergeDeepRight(defaults, inputs)
+    config.externalBucket = not(isNil(config.bucket))
     config.bucket = this.state.bucket || config.bucket
+    config.timestamp = Date.now()
+
+    config.templateS3Key = constructTemplateS3Key(config)
 
     if (isNil(config.bucket)) {
       const awsS3 = await this.load('@serverless/aws-s3')
@@ -20,21 +30,33 @@ class AwsCloudFormation extends Component {
       config.bucket = name
     }
 
-    const outputs = {
-      bucket: config.bucket
+    const { cloudformation, s3 } = getClients()
+
+    let stackOutputs = {}
+    if (await needsUpdate(cloudformation, config)) {
+      await uploadTemplate(s3, config)
+      stackOutputs = await createOrUpdateStack(cloudformation, config)
+    } else {
+      stackOutputs = await fetchOutputs(cloudformation, config)
     }
 
-    const { cloudformation } = getClients()
-    await createOrUpdateStack(cloudformation, config.bucket, inputs)
-
-    this.state = outputs
+    this.state = {
+      bucket: config.bucket,
+      externalBucket: config.externalBucket,
+      region: config.region
+    }
     await this.save()
-
-    return {}
+    return stackOutputs
   }
 
   async remove(inputs = {}) {
     console.log('remove', inputs)
+    await deleteStack()
+    if (not(this.state.externalBucket)) {
+      // delete bucket
+    }
+    this.state = {}
+    await this.save()
     return {}
   }
 }
--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const aws = require('aws-sdk')
-const { isNil } = require('ramda')
+const { reduce, head, merge } = require('ramda')
+const { utils } = require('@serverless/core')
 
 const getClients = (credentials, region = 'us-east-1') => {
   const cloudformation = new aws.CloudFormation({ credentials, region })
@@ -10,9 +11,130 @@ const getClients = (credentials, region = 'us-east-1') => {
   }
 }
 
-const createOrUpdateStack = async (cloudformation, inputs) => {
-  console.log(cloudformation, inputs)
-  return
+const needsUpdate = async (cloudformation, config) => {
+  let deployedTemplate = ''
+  let exists = false
+  try {
+    deployedTemplate = await cloudformation
+      .getTemplate({ StackName: config.stackName, TemplateStage: 'Original' })
+      .promise()
+    exists = true
+  } catch (error) {
+    if (error.message !== `Stack with id ${config.stackName} does not exist`) {
+      throw error
+    }
+    return true
+  }
+  if (deployedTemplate.TemplateBody === JSON.stringify(config.template)) {
+    return false
+  }
+  return true
 }
 
-module.exports = { createOrUpdateStack, getClients }
+const constructTemplateS3Key = (config) => {
+  return `${config.stackName}/${config.timestamp}-${new Date(
+    config.timestamp
+  ).toISOString()}/template.json`
+}
+
+const uploadTemplate = async (s3, config) => {
+  await s3
+    .putObject({
+      Bucket: config.bucket,
+      Key: config.templateS3Key,
+      Body: JSON.stringify(config.template),
+      ContentEncoding: 'application/json',
+      ACL: 'bucket-owner-full-control'
+    })
+    .promise()
+}
+
+const createOrUpdateStack = async (cloudformation, config) => {
+  let deployedTemplate = ''
+  let exists = false
+  try {
+    deployedTemplate = await cloudformation
+      .getTemplate({ StackName: config.stackName, TemplateStage: 'Original' })
+      .promise()
+    exists = true
+  } catch (error) {
+    if (error.message !== `Stack with id ${config.stackName} does not exist`) {
+      throw error
+    }
+  }
+  if (deployedTemplate.TemplateBody === JSON.stringify(config.template)) {
+    return
+  }
+  const params = {
+    StackName: config.stackName,
+    Capabilities: config.capabilities,
+    // EnableTerminationProtection: config.enableTerminationProtection, cloudformation.updateTerminationProtection <- move here
+    RoleARN: config.role,
+    RollbackConfiguration: config.rollbackConfiguration,
+    Parameters: config.parameters,
+    TemplateURL: `https://s3.amazonaws.com/${config.bucket}/${config.templateS3Key}`
+  }
+  if (!exists) {
+    await cloudformation.createStack(params).promise()
+  } else {
+    try {
+      await cloudformation.updateStack(params).promise()
+    } catch (error) {
+      if (error.message !== 'No updates are to be performed.') {
+        throw error
+      }
+    }
+  }
+
+  const stacks = await waitFor(
+    cloudformation,
+    exists ? 'UPDATE_COMPLETE' : 'CREATE_COMPLETE',
+    config
+  )
+
+  return stackOutputsToObject(head(stacks).Outputs)
+}
+
+const waitFor = async (cloudformation, event, config) =>
+  new Promise(async (resolve, reject) => {
+    let inProgress = true
+    do {
+      const { Stacks } = await cloudformation
+        .describeStacks({ StackName: config.stackName })
+        .promise()
+      if (head(Stacks).StackStatus === event) {
+        return resolve(Stacks)
+      } else {
+        await utils.sleep(5000)
+      }
+    } while (inProgress)
+  })
+
+const waitFor2 = async (cloudformation, event, config) =>
+  new Promise((resolve, reject) => {
+    cloudformation.waitFor(event, { StackName: config.stackName }, (error, data) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(data)
+    })
+  })
+
+const fetchOutputs = async (cloudformation, config) => {
+  const { Stacks } = await cloudformation.describeStacks({ StackName: config.stackName }).promise()
+  return stackOutputsToObject(head(Stacks).Outputs)
+}
+
+const stackOutputsToObject = (outputs) =>
+  reduce((acc, { OutputKey, OutputValue }) => merge(acc, { [OutputKey]: OutputValue }), {}, outputs)
+
+const removeStack = async (cloudformation, config) => {}
+
+module.exports = {
+  needsUpdate,
+  fetchOutputs,
+  createOrUpdateStack,
+  constructTemplateS3Key,
+  getClients,
+  uploadTemplate
+}
